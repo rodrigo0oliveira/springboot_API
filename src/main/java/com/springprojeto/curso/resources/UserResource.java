fced25c91c46fb10832b08b516d5d575a72c0505
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-06-04 19:30:14 -0300

    H2 database, test profile, JPA

--- src/main/java/com/springprojeto/curso/resources/UserResource.java
+++ src/main/java/com/springprojeto/curso/resources/UserResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springprojeto.curso.entities.User;
 
 @RestController
-@RequestMapping(value ="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 	
 	@GetMapping
