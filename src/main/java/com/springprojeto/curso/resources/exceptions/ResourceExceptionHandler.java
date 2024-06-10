a998d9a645a76095c701f63a2c9059e91a36dc79
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-06-10 16:49:27 -0300

    Exception created

---
+++ src/main/java/com/springprojeto/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.springprojeto.curso.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.springprojeto.curso.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandartError> resourceNotFound(ResourceNotFoundException e,HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError stardart = new StandartError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(stardart);
+	}
+
+}
--- src/main/java/com/springprojeto/curso/services/UserService.java
+++ src/main/java/com/springprojeto/curso/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.springprojeto.curso.entities.User;
 import com.springprojeto.curso.repository.UserRepository;
+import com.springprojeto.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional <User> user = userRepository.findById(id);
-		return user.get();
+		return user.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user){
