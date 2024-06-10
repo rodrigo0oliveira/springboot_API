e33fff7b2695fae9c5ef3f51682393676aa8e0e8
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-06-10 11:59:16 -0300

    Product - OrderItem association

--- src/main/java/com/springprojeto/curso/entities/Product.java
+++ src/main/java/com/springprojeto/curso/entities/Product.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,10 +31,13 @@ public class Product implements Serializable{
 	private Double price;
 	private String imgUrl;
 	
-	@JsonIgnore
+	
 	@ManyToMany
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	
 	public Product() {
@@ -88,8 +92,18 @@ public class Product implements Serializable{
 		this.imgUrl = imgUrl;
 	}
 	
+	@JsonIgnore
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem obj : items) {
+			set.add(obj.getOrder());
+		}
+		return set;
 	}
 
 	@Override
